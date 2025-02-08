e559d3d9d379cb11219de4d581571d6ff472e981
kazeneza zephilin <caz@kazenezas-MacBook-Pro.local>
2025-02-09 00:07:10 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,53 @@
+const openBtn = document.createElement("button");
+openBtn.classList.add("opener");
+openBtn.textContent = "Open it!";
+document.body.appendChild(openBtn);
+const homeDropDown = document.createElement("ul");
+homeDropDown.classList.add("item-container", "open");
+
+populateDropdownList(
+    homeDropDown,
+    ["Home", "Products", "From us", "Contact"]
+);
+
+function populateDropdownList(dropDown, pages) {
+    if (!dropDown || !pages) {
+        console.log("Please provide the required arguments");
+        return;
+    }
+    if (!Array.isArray(pages)) {
+        console.log("Please provide an array of pages");
+        return;
+    }
+    pages.forEach((page) => {
+        dropDown.appendChild(createListItems("item", page));
+    });
+    document.body.appendChild(dropDown);
+}
+
+function createListItems(classList, text) {
+    if (!classList || !text) {
+        console.log("Please provide the required     arguments");
+        return;
+    }
+
+    const separator = document.createElement("div");
+    const p = document.createElement("p");
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+
+    a.textContent = text;
+    a.href = "#";
+    li.appendChild(a);
+    li.classList.add(classList);
+    p.appendChild(li);
+    separator.classList.add("separator-line");
+    p.appendChild(separator);
+    return p;
+}
+
+openBtn.addEventListener("click", () => {
+    homeDropDown.classList.contains("closed")
+        ? homeDropDown.classList.remove("closed")
+        : homeDropDown.classList.add("closed");
+});
